5f97454669c1e2570a4d01381901851a50b88d3c
Jeffrey <562475805lanu@gmail.com>
2024-05-27 20:39:24 +1000

    Add more layout

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
-import Link from "next/link";
-import Navigation from "./components/Navigation";
-
 export default function Page() {
   return (
     <>
       <h1>The wild Oasis. Welcome to paradise.</h1>
-      {/* <Navigation /> */}
     </>
   );
 }
