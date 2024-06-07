971dc6ddd95ad29c6a4ef9a8b5c5844c271c1cbe
Jeffrey <562475805lanu@gmail.com>
2024-06-07 17:38:56 +1000

    testing hooks

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
-export default function Page() {
+import Counter from "./_components/Counter";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <>
       <h1>The wild Oasis. Welcome to paradise.</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}> {user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={data}></Counter>
     </>
   );
 }
