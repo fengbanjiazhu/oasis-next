d8a2d1a26724bc4983dae78781ae570e803ecfc1
Jeffrey <562475805lanu@gmail.com>
2024-06-10 17:25:20 +1000

    Add fonts, removes babel config due to conflict

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,15 @@ import Navigation from "@/app/_components/Navigation";
 
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+// console.log(josefin);
+
 export const metadata = {
   title: {
     template: "%s | The Wild Oasis",
@@ -15,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <header>
           <Logo />
           <Navigation />
