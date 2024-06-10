16c705f2e6d86b8094d2a62abfdc28c20f1978ae
Jeffrey <562475805lanu@gmail.com>
2024-06-10 17:34:12 +1000

    Add layout css to project

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Header from "./_components/Header";
 
 import "@/app/_styles/globals.css";
 
@@ -24,13 +23,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by @Jeff</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
