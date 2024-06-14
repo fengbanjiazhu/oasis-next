be214beee189e710bad49a24816d36d0be2a87b7
Jeffrey <562475805lanu@gmail.com>
2024-06-14 17:23:31 +1000

    Add error handling components

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,17 +1,16 @@
 import { eachDayOfInterval } from "date-fns";
 
 import { supabase } from "./supabase";
+import { notFound } from "next/navigation";
 /////////////
 // GET
 
 export async function getCabin(id) {
   const { data, error } = await supabase.from("cabins").select("*").eq("id", id).single();
 
-  // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
-
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
