50a934e2d4a19f8e590a49f392a5e1c9014e16a3
Jeffrey <562475805lanu@gmail.com>
2024-06-19 17:09:38 +1000

    re-structure Filter code

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -2,12 +2,31 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-const btnStyle = "px-5 py-2 hover:bg-primary-700";
+const btns = [
+  {
+    title: "All cabins",
+    filter: "all",
+  },
+  {
+    title: "1-3 guests",
+    filter: "small",
+  },
+  {
+    title: "4-7 guests",
+    filter: "medium",
+  },
+  {
+    title: "8-12 guests",
+    filter: "large",
+  },
+];
 
 export default function Filter() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
+
+  const activeFilter = searchParams.get("capacity") ?? "all";
 
   function handleFilter(selection) {
     const params = new URLSearchParams(searchParams);
@@ -17,18 +36,26 @@ export default function Filter() {
 
   return (
     <div className="border border-primary-800 flex">
-      <button className={btnStyle} onClick={() => handleFilter("all")}>
-        All cabins
-      </button>
-      <button className={btnStyle} onClick={() => handleFilter("small")}>
-        1-3 guests
-      </button>
-      <button className={btnStyle} onClick={() => handleFilter("medium")}>
-        4-7 guests
-      </button>
-      <button className={btnStyle} onClick={() => handleFilter("large")}>
-        8-12 guests
-      </button>
+      {btns.map((button) => {
+        return (
+          <Button filter={button.filter} activeFilter={activeFilter} handleFilter={handleFilter}>
+            {button.title}
+          </Button>
+        );
+      })}
     </div>
+  );
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      className={`px-5 py-2 hover:bg-primary-700 ${
+        activeFilter === filter ? "bg-primary-700 text-primary-50" : ""
+      }`}
+      onClick={() => handleFilter(filter)}
+    >
+      {children}
+    </button>
   );
 }
