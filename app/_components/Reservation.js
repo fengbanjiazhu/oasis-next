ed617f1cc36dea868043dc46a65a3bdedd4d634b
Jeffrey <562475805lanu@gmail.com>
2024-06-20 17:35:34 +1000

    break cabin detail page into multiple components

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,5 +1,18 @@
 import React from "react";
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
+import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
 
-export default function Reservation() {
-  return <div>Reservation</div>;
+export default async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className="grid grid-cols-2 border-primary-800 min-h-[400px]">
+      <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
 }
--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -119,6 +119,8 @@ export async function getSettings() {
     throw new Error("Settings could not be loaded");
   }
 
+  // await new Promise((res) => setTimeout(res, 5000));
+
   return data;
 }
 
